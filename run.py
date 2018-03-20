7d7a51c87dea11fe57f77dc67ba146adad213aeb
prajwalchadaga <prajwalchadaga96@gmail.com>
2018-03-20 14:07:39 +0530

    Update run.py

--- run.py
+++ run.py
@@ -4,10 +4,10 @@ a=4
 b=4
 f=0
 x=[]
-os.system("gcc test.cpp")
+os.system("gcc /Baas/blob/master/test.cpp")
 for i in range(a):
   for j in range(b):
-    #subprocess.call('cd /Users/pprajwal/Documents/jenkins', shell=True)
+    subprocess.call('cd /Baas/blob/master', shell=True)
     cmd=['./a.out',str(i),str(j)]
     output = subprocess.Popen( cmd, stdout=subprocess.PIPE ).communicate()[0]
     if not  re.search(str(i+j),output):
