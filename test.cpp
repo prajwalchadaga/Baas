32f9ddedc02416de0dd9b5f4d5af6705ba2fb4d9
prajwalchadaga <prajwalchadaga96@gmail.com>
2018-03-20 14:03:18 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,8 +1,15 @@
+#include <errno.h>   // for errno
+#include <limits.h>  // for INT_MAX
+#include <stdlib.h>  // for strtol
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
 int main(int argc, char** argv )
 {
-int a,b;
-a=int(argv[0])
-b=int(argv[1])
-cout<<a+b;
+
+int  a = atoi(argv[1]);
+int  b = atoi(argv[2]);
+printf("%d\n",a+b*b);
 return 0;
 }
